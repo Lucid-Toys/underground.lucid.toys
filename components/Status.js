6891dbed05d9e65dde7b8414a231fa951d8e5d53
Daniel Eden <dan.eden@me.com>
2019-11-13 16:41:56 +0000

    Add image to differentiate severities

--- components/Status.js
+++ components/Status.js
@@ -1,10 +1,22 @@
+import { css } from "@emotion/core"
 import styled from "@emotion/styled"
 
+const stylePartial = props =>
+  props.severity < 10
+    ? css`
+        padding-left: 1em;
+        background-image: url("/images/warning.svg");
+        background-repeat: no-repeat;
+        background-position: 0 65%;
+      `
+    : null
+
 const StyledStatus = styled.div`
-  font-variant-caps: all-small-caps;
-  font-size: 0.875em;
-  font-weight: 700;
+  font-size: 0.75em;
+  font-weight: 600;
   opacity: 0.75;
+  margin: 0.25em 0;
+  ${stylePartial};
 `
 
 export default function Status({ children, severity }) {

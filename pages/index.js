962ecdafbeee6eeac5bf8186856c1de97b1986ce
Daniel Eden <dan.eden@me.com>
2019-11-20 11:30:33 +0000

    Revert "Add Google Analytics"
    This reverts commit 63ffbbefca6100b99f4a9bacfaa35e169d70efc8.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 import styled from "@emotion/styled"
-import { useEffect } from "react"
-import ReactGA from "react-ga"
 import useSWR from "swr"
 import NetworkIndicator from "../components/NetworkIndicator"
 import TFLLineStatusList from "../components/TFLLineStatusList"
 import Timeline from "../components/Timeline"
 import fetcher from "../utils/fetcher"
-
-ReactGA.initialize("UA-122108242-2")
 
 const AppContainer = styled.main`
   display: flex;
@@ -18,11 +14,6 @@ const Index = ({ initialData }) => {
   const { data } = useSWR("/api/data", fetcher, {
     refreshInterval: 3000,
     initialData,
-  })
-
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search)
-    return () => null
   })
 
   return data ? (

f2b1a9f1432d18416a95864885d49e7dd8c94be8
Daniel Eden <dan.eden@me.com>
2019-12-20 18:39:03 +0000

    Remove redundant line name from status severity reasons

--- pages/api/data.ts
+++ pages/api/data.ts
@@ -44,6 +44,50 @@ export default (request: NextApiRequest, result: NextApiResponse): void => {
   fetch(API_URL)
     .then(response => response.json())
     .then((data: TfLAPIResponse[]) => {
+      /**
+       * Line severity status descriptions often begin with the name of the
+       * line. This is somewhat redundant given that in our context, the
+       * name of the line will always be visible; so we'll remove it from the
+       * beginning so that important information about the line's status is
+       * more visible on e.g. watch faces
+       */
+      const transformedData = data.map(datum => {
+        const transformedLineStatuses = datum.lineStatuses.map(lineStatus => {
+          const lineName = datum.name.replace('&', 'and')
+          const matchingRegex = new RegExp(`${lineName}( Line)?\: ?`)
+          const transformedReason = lineStatus.reason?.replace(
+            matchingRegex,
+            ''
+          )
+          /**
+           * Return the new reason for each line status
+           */
+          return {
+            ...lineStatus,
+            reason: transformedReason,
+          }
+        })
+
+        /**
+         * Return the new array of line statuses
+         */
+        return {
+          ...datum,
+          lineStatuses: transformedLineStatuses,
+        }
+      })
+
+      /**
+       * And finally return the transformed set of TfLAPIResponses
+       */
+      return transformedData
+    })
+    .then((data: TfLAPIResponse[]) => {
+      /**
+       * We want to sort the lines in two ways:
+       * 1. By status severity
+       * 2. Alphabetically
+       */
       const sortedData = data
         .slice()
         .sort((a: TfLAPIResponse, b: TfLAPIResponse) => {

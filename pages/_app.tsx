5990cb6a7a1bc75355e71e8d116e28ef87af2f5d
Daniel Eden <dan.eden@me.com>
2020-01-16 09:48:20 +0000

    Remove `getInitialProps` from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,17 +7,6 @@ import * as gtag from '../utils/gtag'
 Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
 export default class UndergroundApp extends App {
-  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-  static async getInitialProps({ Component, ctx }) {
-    let pageProps = {}
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
-    }
-
-    return { pageProps }
-  }
-
   render(): ReactElement {
     const { Component, pageProps } = this.props
 

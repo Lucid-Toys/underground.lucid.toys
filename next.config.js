ec67c757ad01b219e98073b94b3a81741454288e
Daniel Eden <dan.eden@me.com>
2022-07-20 11:24:53 +0100

    Fix config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
-const withImages = require('next-images')
 
-module.exports = withImages(
-  withPWA({
-    reactStrictMode: true,
-    pwa: {
-      dest: 'public',
-      runtimeCaching,
-    },
-  })
-)
+module.exports = withPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+})
